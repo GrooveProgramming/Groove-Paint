b1fd2b4f4ae884740e0d16addc1d82520316f61d
B294 <64226546+B294@users.noreply.github.com>
2020-05-31 12:54:44 -0700

    Fixed most paint editor glitches

--- index.js
+++ index.js
@@ -55,3 +55,8 @@ function draw(e) {
 
   ctx.stroke(); 
 }
+var wrapper = document.getElementById("signature-pad");
+var canvas = wrapper.querySelector("canvas");
+var ratio = Math.max(window.devicePixelRatio || 1, 1);
+canvas.width = canvas.offsetWidth * ratio;
+canvas.height = canvas.offsetHeight * ratio;
